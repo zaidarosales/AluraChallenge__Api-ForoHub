380a3ba2fcc9a99864a359c31baed9eb2afaae0b
Zaida Rosales <zaidarosales16@gmail.com>
2025-07-29 12:46:02 -0500

    Creación de la estructura principal del proyecto

---
+++ src/main/java/com/alurachallenge/Api_ForoHub/ApiForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.Api_ForoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alurachallenge/Api_ForoHub/ApiForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.Api_ForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
